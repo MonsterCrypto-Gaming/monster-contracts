51f287a7260fc013d35fe6f8a1792d683827e33d
Freddie von Stange <freddie71010@gmail.com>
2022-05-17 18:30:53 -0400

    Create MONToken contract and deployment code

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,17 @@
 from tracemalloc import start
-from brownie import MonsterPortal, accounts, config, network
+from brownie import MONToken, MonsterPortal, accounts, config, network
 from .utils import get_account, LOCAL_BLOCKCHAIN_ENVS, get_contract, fund_with_link, print_line
 import time
 
+
+def deploy_monster_token():
+    account = get_account()
+    monster_token = MONToken.deploy(
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False),
+    )
+    print_line("Deployed Monster ($MON) tokens!")
+    return monster_token
 
 def deploy_monsterportal():
     account = get_account()
@@ -14,6 +23,7 @@ def deploy_monsterportal():
     return monster_portal
 
 def main():
+    # deploy_monster_token()
     deploy_monsterportal()
 
 

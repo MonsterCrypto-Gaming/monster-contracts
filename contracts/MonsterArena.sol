0e0d264f330f9f77331b7ff3f578e9a7eb29c921
Freddie von Stange <freddie71010@gmail.com>
2022-05-17 16:19:50 -0400

    copy over 3 contracts

---
+++ contracts/MonsterArena.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+/// @title MonsterArena
+/// @notice This contract provides the ability to compete with other addresses or the house
+
+contract MonsterArena {
+
+}

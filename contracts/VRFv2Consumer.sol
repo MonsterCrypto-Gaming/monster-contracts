510559d5168813ec983f1ad9cb8b07502a04ed46
Freddie von Stange <freddie71010@gmail.com>
2022-05-18 23:42:25 -0400

    WIP -- refactor Chainlink VRF contract

--- contracts/VRFv2Consumer.sol
+++ contracts/VRFv2Consumer.sol
@@ -9,17 +9,17 @@ contract VRFv2Consumer is VRFConsumerBaseV2 {
     VRFCoordinatorV2Interface COORDINATOR;
 
     // Your subscription ID.
-    uint64 s_subscriptionId;
+    // uint64 s_subscriptionId;
 
     // Rinkeby coordinator. For other networks,
     // see https://docs.chain.link/docs/vrf-contracts/#configurations
-    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    // address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
 
     // The gas lane to use, which specifies the maximum gas price to bump to.
     // For a list of available gas lanes on each network,
     // see https://docs.chain.link/docs/vrf-contracts/#configurations
-    bytes32 keyHash =
-        0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+    // bytes32 keyHash =
+    //     0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
 
     // Depends on the number of requested values that you want sent to the
     // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
@@ -36,22 +36,31 @@ contract VRFv2Consumer is VRFConsumerBaseV2 {
     // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
     uint32 numWords = 1;
 
-    uint256[] public s_randomWordExpanded;
-    uint256[] public s_randomWord;
+    uint256[] private s_randomNumSplit;
+    uint256[] public s_randomNum;
     uint256 public s_requestId;
+    uint256 public s_splitBy = 4;
+
     address s_owner;
 
-    constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
-        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+    event ReceiveRandomness(uint256[] numReceived);
+    event SplitRandomNumber(uint256[] numToSplit);
+
+    constructor(
+        uint64 _subscriptionId,
+        address _vrfCoordinator,
+        bytes32 _keyHash
+    ) VRFConsumerBaseV2(_vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
         s_owner = msg.sender;
-        s_subscriptionId = subscriptionId;
+        s_subscriptionId = _subscriptionId;
     }
 
     // Assumes the subscription is funded sufficiently.
     function requestRandomWords() external onlyOwner {
         // Will revert if subscription is not set and funded.
         s_requestId = COORDINATOR.requestRandomWords(
-            keyHash,
+            _keyHash,
             s_subscriptionId,
             requestConfirmations,
             callbackGasLimit,
@@ -61,27 +70,34 @@ contract VRFv2Consumer is VRFConsumerBaseV2 {
 
     function fulfillRandomWords(
         uint256, /* requestId */
-        uint256[] memory randomWords
+        uint256[] memory _randomness
     ) internal override {
-        s_randomWord = randomWords;
+        s_randomNum = _randomness;
+        emit ReceiveRandomness(s_randomNum);
+        s_randomNumSplit = expand(s_randomNum[0], s_splitBy);
+        emit SplitRandomNumber(s_randomNumSplit);
     }
 
-    function expandRandomNumber(uint256 s_num, uint256 n) public {
-        s_randomWordExpanded = expand(s_num, n);
-    }
-
-    function expand(uint256 s_num, uint256 n)
-        public
+    function expand(uint256 num, uint256 n)
+        internal
         pure
         returns (uint256[] memory expandedValues)
     {
         expandedValues = new uint256[](n);
         for (uint256 i = 0; i < n; i++) {
             expandedValues[i] =
-                (uint256(keccak256(abi.encode(s_num, i))) % 100) +
+                (uint256(keccak256(abi.encode(num, i))) % 100) +
                 1;
         }
         return expandedValues;
+    }
+
+    function getCardRandomizerNumbers()
+        external
+        view
+        returns (uint256[] memory)
+    {
+        return s_randomNumSplit;
     }
 
     modifier onlyOwner() {
